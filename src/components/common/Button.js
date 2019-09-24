6a15ed02397218aa5220f8dedf1af31e29affa3d
田原 一樹 <ssp0727.lnc@gmail.com>
2019-09-24 20:53:12 +0900

    [update]

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -2,6 +2,7 @@ import styled, { css } from 'styled-components'
 
 const Button = styled.button`
   background: ${props => props.theme.backgroundColor}
+  border: 5px solid ${props => props.theme.primaryColor}
 `
 
 export default Button
